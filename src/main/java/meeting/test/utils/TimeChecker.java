db71241f95545cfedd6e57df5d5bfbda2e5e1ef3
berkam <berkam15@mail.ru>
2020-06-15 17:45:33 +0300

    add logs in TimeChecker and change log level

--- src/main/java/meeting/test/utils/TimeChecker.java
+++ src/main/java/meeting/test/utils/TimeChecker.java
@@ -1,5 +1,6 @@
 package meeting.test.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import meeting.test.entity.Meeting;
 import meeting.test.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class TimeChecker {
 
@@ -42,18 +44,26 @@ public class TimeChecker {
     }
 
     private boolean checkBeginTimeBeforeAfterTime(long timeBegin, long timeEnd) {
+        log.debug("Method checkBeginTimeBeforeAfterTime, timeBegin {}, timeEnd {}", timeBegin, timeEnd);
+        log.debug("result {}", timeBegin < timeEnd);
         return timeBegin < timeEnd;
     }
 
     private boolean checkTimeIsFromFuture(long timeBegin, long timeEnd) {
+        log.debug("Method checkTimeIsFromFuture, timeBegin {}, timeEnd {}, currentTimeMillis {}", timeBegin, timeEnd, System.currentTimeMillis());
+        log.debug("result {}", (timeBegin > System.currentTimeMillis()) && (timeEnd > System.currentTimeMillis()));
         return timeBegin > System.currentTimeMillis() && timeEnd > System.currentTimeMillis();
     }
 
     private boolean checkMeetingMoreThanMin(long timeBegin, long timeEnd) {
+        log.debug("Method checkMeetingMoreThanMin, timeBegin {}, timeEnd {}, value {}", timeBegin, timeEnd, propertiesConfiguration.getMinMeetingTime() * 1000 * 60);
+        log.debug("result {}", (timeEnd - timeBegin) > (propertiesConfiguration.getMinMeetingTime() * 1000 * 60));
         return timeEnd - timeBegin > propertiesConfiguration.getMinMeetingTime() * 1000 * 60;
     }
 
     private boolean checkMeetingLessThanMax(long timeBegin, long timeEnd) {
-        return timeEnd - timeBegin > propertiesConfiguration.getMinMeetingTime() * 1000 * 60 * 60;
+        log.debug("Method checkMeetingLessThanMax, timeBegin {}, timeEnd {}, value {}", timeBegin, timeEnd, propertiesConfiguration.getMinMeetingTime() * 1000 * 60 * 60);
+        log.debug("result {}", timeEnd - timeBegin < propertiesConfiguration.getMaxMeetingTime() * 1000 * 60 * 60);
+        return timeEnd - timeBegin < propertiesConfiguration.getMaxMeetingTime() * 1000 * 60 * 60;
     }
 }
